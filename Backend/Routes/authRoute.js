4010df067807b7385c67a69339770c2f30b0ea7f
Prince Vishwakarma <prince.vishwakarma392@gmail.com>
2026-07-31 21:58:49 +0530

    add rate limiting to login and signup routes

--- Backend/Routes/authRoute.js
+++ Backend/Routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { signin, signout, signup } from "../Controller/authController.js"
+import { authLimiter } from "../RateLimiters/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/",signup);
-router.get("/",signin);
+router.post("/", authLimiter, signup);
+router.get("/", authLimiter, signin);
 router.get("/logout",signout)
 
 
